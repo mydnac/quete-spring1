8b8618158103b3943518f54b78b26646cbb9b60a
mydnac <mandycoelho5@gmail.com>
2020-05-10 17:00:15 +0200

    create 3 routes doctor

---
+++ src/main/java/com/wildcodeschool/quetespring1/QueteSpring1Application.java
@@ -0,0 +1,13 @@
+package com.wildcodeschool.quetespring1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QueteSpring1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QueteSpring1Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/wildcodeschool/quetespring1/QueteSpring1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wildcodeschool.quetespring1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QueteSpring1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
